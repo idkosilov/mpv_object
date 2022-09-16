2766f7b3ebc624dc40dd686f48774a1160dcea50
Ivan Kosilov <idkosilov@gmail.com>
2022-09-17 01:38:18 +0700

    Glfw initialization moved to separate method

--- render_context.py
+++ render_context.py
@@ -10,7 +10,6 @@ class RenderContext:
         self._context = self._current_opengl_context()
         self._surface = QOffscreenSurface()
         self._surface.create()
-        print(self._surface)
 
     def _current_opengl_context(self):
         operating_system = platform.system()
@@ -49,18 +48,12 @@ class RenderContext:
         raise 'Cannot initialize OpenGL'
 
     def windows_context(self):
-        import glfw
-
-        if not glfw.init():
-            raise 'Cannot initialize OpenGL'
-
-        glfw.window_hint(glfw.VISIBLE, glfw.FALSE)
-        window = glfw.create_window(1, 1, "mpvQC-OpenGL", None, None)
-
-        glfw.make_context_current(window)
-        context = QOpenGLContext.currentContext()
-        context.makeCurrent(self._surface)
-        return glfw.get_proc_address
+        try:
+            from OpenGL import GLX
+            return self.glfw_implementation
+        except AttributeError:
+            pass
+        raise 'Cannot initialize OpenGL'
 
     @staticmethod
     def glut_implementation(name: bytes):
@@ -76,6 +69,21 @@ class RenderContext:
     def egl_implementation(name: bytes):
         from OpenGL import EGL
         return EGL.eglGetProcAddress(name)
+
+    def glfw_implementation(self, name: bytes):
+        import glfw
+
+        if not glfw.init():
+            raise 'Cannot initialize OpenGL'
+
+        glfw.window_hint(glfw.VISIBLE, glfw.FALSE)
+        window = glfw.create_window(1, 1, "mpvQC-OpenGL", None, None)
+
+        glfw.make_context_current(window)
+        context = QOpenGLContext.currentContext()
+        context.makeCurrent(self._surface)
+        return glfw.get_proc_address(name)
+
 
 
 
